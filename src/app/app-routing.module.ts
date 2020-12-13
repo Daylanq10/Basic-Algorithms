0163350e272c33ad3ead0e018bd2b013c8ac37d5
Daylan Quinn <daylanq10@gmail.com>
2020-12-12 23:29:46 -0600

    First push of website
    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeInfoComponent} from './home-info/home-info.component';
+import { LinearSearchComponent} from './linear-search/linear-search.component';
+import { BinarySearchComponent} from './binary-search/binary-search.component';
+import { SelectionSortComponent} from './selection-sort/selection-sort.component';
+import { BubbleSortComponent} from './bubble-sort/bubble-sort.component';
+import { MergeSortComponent} from './merge-sort/merge-sort.component';
+import { QuickSortComponent} from './quick-sort/quick-sort.component';
+
+const routes: Routes = [
+  {path: 'home-info', component: HomeInfoComponent},
+  {path: 'linear-search', component: LinearSearchComponent},
+  {path: 'binary-search', component: BinarySearchComponent},
+  {path: 'selection-sort', component: SelectionSortComponent},
+  {path: 'bubble-sort', component: BubbleSortComponent},
+  {path: 'merge-sort', component: MergeSortComponent},
+  {path: 'quick-sort', component: QuickSortComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
