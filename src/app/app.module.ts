4a4d4ea7e94d4ee93e9953a102f57da9668813d7
Daylan Quinn <daylanq10@gmail.com>
2020-12-12 23:26:30 -0600

    merge to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeInfoComponent } from './home-info/home-info.component';
+import { LinearSearchComponent } from './linear-search/linear-search.component';
+import { BinarySearchComponent } from './binary-search/binary-search.component';
+import { SelectionSortComponent } from './selection-sort/selection-sort.component';
+import { BubbleSortComponent } from './bubble-sort/bubble-sort.component';
+import { MergeSortComponent } from './merge-sort/merge-sort.component';
+import { QuickSortComponent } from './quick-sort/quick-sort.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeInfoComponent,
+    LinearSearchComponent,
+    BinarySearchComponent,
+    SelectionSortComponent,
+    BubbleSortComponent,
+    MergeSortComponent,
+    QuickSortComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+          {path: 'home-info', component: HomeInfoComponent},
+          {path: 'linear-search', component: LinearSearchComponent}
+        ]),
+        AppRoutingModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
